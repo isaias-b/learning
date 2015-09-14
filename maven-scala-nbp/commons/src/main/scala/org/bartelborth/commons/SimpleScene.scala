89fa07904bbce04a8df1286a769f1f12070626b2
Isaias Bartelborth <isaias.b@gmx.de>
2015-09-14 22:11:35 +0200

    Implement triple buffering with motion blur

--- maven-scala-nbp/commons/src/main/scala/org/bartelborth/commons/SimpleScene.scala
+++ maven-scala-nbp/commons/src/main/scala/org/bartelborth/commons/SimpleScene.scala
@@ -20,14 +20,36 @@ import java.awt.Color
 import java.awt.Graphics
 import java.awt.Graphics2D
 import java.awt.RenderingHints
+import java.awt.event.ComponentAdapter
+import java.awt.event.ComponentEvent
 import java.awt.geom.Line2D
 import java.awt.geom.Point2D
-import javax.swing.JPanel
+import java.awt.image.BufferedImage
+import javax.swing.JComponent
 
-class SimpleScene extends JPanel with Scene {
+class SimpleScene extends JComponent with Scene {
   private var t = 0.0d
+  import Color._
+  object resizer extends ComponentAdapter {
+    private[this] def newImage = new BufferedImage(w, h, BufferedImage.TYPE_4BYTE_ABGR)
+    def createImage =
+      if (w <= 0 || h <= 0) None
+      else Some(newImage)
+    override def componentResized(e: ComponentEvent) = {
+      buffer = createImage.map(b => {
+        val g = b.createGraphics
+        g.setColor(white)
+        g.fillRect(0, 0, w + 2 * MARGIN - 1, h + 2 * MARGIN - 1)
+        g.dispose
+        b
+      })
+    }
+  }
+  private var buffer = resizer.createImage
 
   setDoubleBuffered(true)
+  //  setOpaque(true)
+  addComponentListener(resizer)
 
   val tx: F = sin(1)
   val ty: F = cos(1)
@@ -41,14 +63,10 @@ class SimpleScene extends JPanel with Scene {
   val incrementalA = ContinuousSeries(square(200))
   val incrementalB = ContinuousSeries(square(200) compose translate(0.25 / 200))
 
-  private val series = Series.define(List(
-    incrementalA,
-    incrementalB
-  ))
-
   def animate(t: Double) = {
     this.t = t
-    series.foreach(_.put(t, w))
+    incrementalA.put(t, w - 125)
+    incrementalB.put(t, w - 125)
     repaint()
   }
 
@@ -59,33 +77,44 @@ class SimpleScene extends JPanel with Scene {
   def cx = w / 2
   def cy = h / 2
 
+  def setRenderingHints(g: Graphics2D) = {
+    import RenderingHints._
+    g.setRenderingHint(KEY_ANTIALIASING, VALUE_ANTIALIAS_ON)
+    g.setRenderingHint(KEY_FRACTIONALMETRICS, VALUE_FRACTIONALMETRICS_ON)
+    g.setRenderingHint(KEY_RENDERING, VALUE_RENDER_QUALITY)
+  }
+
   override def paintComponent(graphics: Graphics) = {
-    val g = graphics.asInstanceOf[Graphics2D]
-    g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
-    g.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON)
-    g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY)
+    super.paintComponent(graphics)
+    buffer.foreach(b => {
+      val g = b.createGraphics
+      setRenderingHints(g)
 
-    super.paintComponent(g)
+      g.setColor(new Color(255, 255, 255, 64))
+      g.fillRect(0, 0, w - 1, h - 1)
 
-    g.translate(MARGIN, MARGIN)
+      g.setColor(new Color(0, 0, 0, 255))
 
-    g.translate(50, 50)
-    g.draw(new Line2D.Double(0, 0, tx(t) * 50 - 25, ty(t) * 50 - 25))
-    g.drawOval(-25, -25, 50, 50)
+      g.translate(50, 50)
+      g.draw(new Line2D.Double(0, 0, tx(t) * 50 - 25, ty(t) * 50 - 25))
+      g.drawOval(-25, -25, 50, 50)
 
-    g.translate(50, -25)
-    g.setColor(Color.red)
-    incrementalA.paintComponent(45, g)
-    g.translate(0, 5)
-    g.setColor(Color.blue)
-    incrementalB.paintComponent(45, g)
-    g.translate(0, -5)
-    g.translate(-50, +25)
+      g.translate(50, -25)
+      g.setColor(red)
+      incrementalA.paintComponent(45, g)
+      g.translate(0, 5)
+      g.setColor(blue)
+      incrementalB.paintComponent(45, g)
+      g.translate(0, -5)
+      g.translate(-50, +25)
 
-    g.translate(-50, -50)
+      g.translate(-50, -50)
 
-    g.drawString("t   = %2.3f".format(t), 12, h - 12)
+      g.drawString("t   = %2.3f".format(t), 12, h - 12)
 
-    g.translate(-MARGIN, -MARGIN)
+      g.dispose()
+
+      graphics.drawImage(b, MARGIN, MARGIN, w, h, null)
+    })
   }
 }
